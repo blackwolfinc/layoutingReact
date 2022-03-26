482d200c5e2c2672c208f4cf285c56de496659f2
Black Wolf Team <47728533+blackwolfinc@users.noreply.github.com>
2022-03-26 13:37:59 +0700

    add looping

---
+++ src/Pages/Loop/Loop.js
@@ -0,0 +1,80 @@
+import React from "react";
+
+export const Loop = () => {
+  let array = [
+        {
+          "_id": "623eb42ab19dcc7392a9b92d",
+          "index": 0,
+          "age": 38,
+          "name": "Robyn Maxwell",
+          "gender": "female",
+          "email": "robynmaxwell@centree.com"
+        },
+        {
+          "_id": "623eb42aedfa469915ead67a",
+          "index": 1,
+          "age": 33,
+          "name": "Newton Stephenson",
+          "gender": "male",
+          "email": "newtonstephenson@centree.com"
+        },
+        {
+          "_id": "623eb42a150b148fb2346adb",
+          "index": 2,
+          "age": 26,
+          "name": "Mendoza Burt",
+          "gender": "male",
+          "email": "mendozaburt@centree.com"
+        },
+        {
+          "_id": "623eb42a80c12c6581519519",
+          "index": 3,
+          "age": 32,
+          "name": "Tyson Wilder",
+          "gender": "male",
+          "email": "tysonwilder@centree.com"
+        },
+        {
+          "_id": "623eb42af63f9d3083bad67d",
+          "index": 4,
+          "age": 31,
+          "name": "Cole Harding",
+          "gender": "male",
+          "email": "coleharding@centree.com"
+        },
+        {
+          "_id": "623eb42a3f085fb8c3713e4b",
+          "index": 5,
+          "age": 20,
+          "name": "Weber Dickson",
+          "gender": "male",
+          "email": "weberdickson@centree.com"
+        },
+        {
+          "_id": "623eb42a87b97fa65fe446ce",
+          "index": 6,
+          "age": 23,
+          "name": "Merle Pickett",
+          "gender": "female",
+          "email": "merlepickett@centree.com"
+        }
+  ];
+
+  const Loop = (array) => {
+    let DataArray = array;
+    return DataArray.map((value) => {
+      return <div>
+        <h1>{value.name}</h1>
+        <h1>{value.gender}</h1>
+        <h1>{value.email}</h1>
+        <hr></hr>
+      </div>;
+    });
+  };
+
+  return (
+    <div>
+    {Loop(array)}
+    </div>
+  );
+};
--- src/Routes/Routers.js
+++ src/Routes/Routers.js
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import { AboutUs } from '../Pages/AboutUs/AboutUs';
 import { ContactUs } from '../Pages/ContactUs/ContactUs';
 import { Hompage } from '../Pages/Hompage/Hompage';
+import { Loop } from '../Pages/Loop/Loop';
 
 
 export const Routers = () => {
@@ -11,6 +12,7 @@ export const Routers = () => {
         <Route path="/" element={<Hompage/>} />
         <Route path="about" element={<AboutUs/>} />
         <Route path="akuganteng" element={<ContactUs/>} />
+        <Route path="looping" element={<Loop/>} />
     </Routes>
   )
 }
